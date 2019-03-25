91cfddb4ca1c25ce17c9614764ca87b1c4f6f118
Pradeep Ravichandran <pradeep.ravichandran@mavs.uta.edu>
2019-03-25 13:59:30 -0500

    Stock Details

---
+++ StockTradeNews/app/src/main/java/com/example/prady/stocktrade_news/Stock_Details.java
@@ -0,0 +1,13 @@
+package com.example.prady.stocktrade_news;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Stock_Details extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_stock__details);
+    }
+}
