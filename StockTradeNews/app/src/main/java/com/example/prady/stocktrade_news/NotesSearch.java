77d8ca3b14d425acf1485208036a1f411a3c19dc
Krishnan Ayyappan <krishnan.ayyappan13@gmail.com>
2019-04-15 21:39:54 -0500

    Notes Search

---
+++ StockTradeNews/app/src/main/java/com/example/prady/stocktrade_news/NotesSearch.java
@@ -0,0 +1,52 @@
+package com.example.prady.stocktrade_news;
+
+import android.app.SearchManager;
+import android.content.Intent;
+import android.database.Cursor;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.example.prady.stocktrade_news.db.NoteHelper;
+
+public class NotesSearch extends AppCompatActivity {
+
+//    private static final Intent Intent = ;
+    NoteHelper nhelp = new NoteHelper(this);
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_notes_search);
+
+        handleIntent(getIntent());
+    }
+
+    /*@Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_notes_search);
+
+        // Get the intent, verify the action and get the query
+        Intent intent = getIntent();
+        if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
+            String query = intent.getStringExtra(SearchManager.QUERY);
+            Cursor cur = nhelp.getWordMatches(query, null);
+        }
+    }*/
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        handleIntent(intent);
+    }
+
+    private void handleIntent(Intent intent) {
+
+        if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
+            String query = intent.getStringExtra(SearchManager.QUERY);
+            //use the query to search your data somehow
+            Cursor cur = nhelp.getWordMatches(query, null);
+//            Log.i("Cursor", cur.toString());
+        }
+    }
+}
